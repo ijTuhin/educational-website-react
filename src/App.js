e72ef6dd262782685ce1ecf30f36cdf8877055eb
ijTuhin <isratishu0209@gmail.com>
2022-04-23 21:44:55 +0600

    img logo added__sub nav created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Header></Header>
       <Routes>
         <Route path='/'></Route>
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,16 +2,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import logo from '../../images/iiuc-logo.png';
 import './Header.css';
 import '../../index.css';
 const Header = () => {
     return (
-        <div className='my-4 text-lg '>
-            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/'>Home</Link>
-            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/academics'>Academics</Link>
-            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/courses'>Courses</Link>
-            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/campus'>Campus</Link>
-            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/search'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Link>
+        <div className='my-4 text-lg flex justify-between items-center mx-4'>
+            <img src={logo} className="w-14" alt="" />
+            <nav>
+                <Link className='nav mx-2 px-1 text-slate-500 hover:text-green-600' to='/'>Home
+                    <ul className='sub-nav absolute bg-slate-400/10 px-3 mt-2'>
+                        <li className='text-slate-500 hover:text-green-600 decoration-4 hover:no-underline'>Section-1</li>
+                        <li className='text-slate-500 hover:text-green-600 decoration-4 hover:no-underline'>Section-2</li>
+                        <li className='text-slate-500 hover:text-green-600 decoration-4 hover:no-underline'>Section-3</li>
+                        <li className='text-slate-500 hover:text-green-600 decoration-4 hover:no-underline'>Section-4</li>
+                        <li className='text-slate-500 hover:text-green-600 decoration-4 hover:no-underline'>Section-5</li>
+                    </ul>
+                </Link>
+                <Link className='mx-2 px-1 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/academics'>Academics</Link>
+                <Link className='mx-2 px-1 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/courses'>Courses</Link>
+                <Link className='mx-2 px-1 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/campus'>Campus</Link>
+                <Link className='mx-2 px-1 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/search'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Link>
+            </nav>
         </div>
     );
 };
