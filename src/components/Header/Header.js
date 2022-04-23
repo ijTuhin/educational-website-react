c35b161c0e4fbed15d21f2364e3f566fd0557da6
ijTuhin <isratishu0209@gmail.com>
2022-04-23 17:42:20 +0600

    header navbar created

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,14 +3,15 @@ import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
+import '../../index.css';
 const Header = () => {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/academics'>Academics</Link>
-            <Link to='/courses'>Courses</Link>
-            <Link to='/campus'>Campus</Link>
-            <Link to='/search'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Link>
+        <div className='my-4 text-lg '>
+            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/'>Home</Link>
+            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/academics'>Academics</Link>
+            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/courses'>Courses</Link>
+            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/campus'>Campus</Link>
+            <Link className='mx-2 text-slate-500 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/search'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Link>
         </div>
     );
 };
