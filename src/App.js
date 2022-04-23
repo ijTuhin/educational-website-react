5da637aa207259d69010fed9cdfd034d0ed7ccb2
ijTuhin <isratishu0209@gmail.com>
2022-04-24 02:23:52 +0600

    header section absolute

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './components/Home/Home';
 
 function App() {
   return (
-    <div>
+    <div className='relative'>
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
