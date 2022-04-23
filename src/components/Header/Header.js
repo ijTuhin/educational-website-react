465537921920d07757b9fca4e78aab69e9170212
ijTuhin <isratishu0209@gmail.com>
2022-04-23 16:58:21 +0600

    FontAwesome React installed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css';
@@ -8,7 +10,7 @@ const Header = () => {
             <Link to='/academics'>Academics</Link>
             <Link to='/courses'>Courses</Link>
             <Link to='/campus'>Campus</Link>
-            <Link to='/search'>Search</Link>
+            <Link to='/search'><FontAwesomeIcon icon={faSearch}></FontAwesomeIcon></Link>
         </div>
     );
 };
