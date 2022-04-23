68547a8049fee546bb073b186384b8fbfe80ae71
ijTuhin <isratishu0209@gmail.com>
2022-04-24 01:20:56 +0600

    added carousel on home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
+import Home from './components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header></Header>
       <Routes>
-        <Route path='/'></Route>
+        <Route path='/' element={<Home></Home>}></Route>
       </Routes>
     </div>
   );
