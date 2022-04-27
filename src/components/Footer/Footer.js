46bbe791b046baa0a6ab2f18dc9de6434383c572
ijTuhin <isratishu0209@gmail.com>
2022-04-27 20:13:05 +0600

    quick link section created

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,17 +3,95 @@ import logo from '../../images/iiuc-logo.png';
 import './Footer.css';
 const Footer = () => {
     return (
-        <div className='flex flex-col items-center bg-slate-900 w-screen h-96'>
-            <div className='w-20'>
+        <div className='flex flex-col justify-between bg-slate-900 w-full h-96 py-8'>
+            <div className='w-20 mx-auto'>
                 <img src={logo} alt="" />
             </div>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className='text-white flex flex-row justify-evenly items-center'>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
+                <div className='flex flex-col'>
+                    <h3>IIUC</h3>
+                    <ul>
+                            <li>Section-1</li>
+                            <li>Section-2</li>
+                            <li>Section-3</li>
+                            <li>Section-4</li>
+                            <li>Section-5</li>
+                            <li>Section-6</li>
+                            <li>Section-7</li>
+                            <li>Section-8</li>
+                            <li>Section-9</li>
+                    </ul>
+                </div>
             </div>
             <div></div>
         </div>
