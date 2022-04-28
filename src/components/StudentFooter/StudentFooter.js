d584a3d022cf2e40ba9e57c7d16165247923dd88
ijTuhin <isratishu0209@gmail.com>
2022-04-28 22:26:05 +0600

    footer section of student panel done

--- src/components/StudentFooter/StudentFooter.js
+++ src/components/StudentFooter/StudentFooter.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import logo from '../../images/iiuc-logo.png';
 
 const StudentFooter = () => {
     return (
-        <div className='w-screen bg-slate-800 text-white'>
-            <h2>Footer</h2>
+        <div className='text-white flex flex-col justify-between items-center bg-slate-900 w-full pt-8'>
+            <img className='w-12' src={logo} alt="" />
+            <h4 className='py-4 text-base text-slate-300 font-semibold'>International Islamic University Chittagong | Kumira, Chattogram-4318 | +8802334461900-7</h4>
+            <h4 className='pb-4 text-xs'>Copyright © 2022 All rights reserved IIUC and <span>Developed by</span> </h4>
         </div>
     );
 };
