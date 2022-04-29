9d37cc1466ccbe5b4dc19a3ba78415fdf1a12ae5
ijTuhin <isratishu0209@gmail.com>
2022-04-29 13:29:41 +0600

    student panel footer made responsive

--- src/components/StudentFooter/StudentFooter.js
+++ src/components/StudentFooter/StudentFooter.js
@@ -5,8 +5,8 @@ const StudentFooter = () => {
     return (
         <div className='text-white flex flex-col justify-between items-center bg-slate-900 w-full pt-8'>
             <img className='w-12' src={logo} alt="" />
-            <h4 className='py-4 text-base text-slate-300 font-semibold'>International Islamic University Chittagong | Kumira, Chattogram-4318 | +8802334461900-7</h4>
-            <h4 className='pb-4 text-xs'>Copyright © 2022 All rights reserved IIUC and <span>Developed by</span> </h4>
+            <h4 className='py-4 text-base text-center text-slate-300 font-semibold'>International Islamic University Chittagong | Kumira, Chattogram-4318 | +8802334461900-7</h4>
+            <h4 className='pb-4 text-xs text-center'>Copyright © 2022 All rights reserved IIUC and <span>Developed by</span> </h4>
         </div>
     );
 };
