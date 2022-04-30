f5f25bb216e33622be5d51d24c6c8193de14d6b1
ijTuhin <isratishu0209@gmail.com>
2022-04-30 16:13:03 +0600

    protected route set for student panel

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 import StudentPanel from './components/StudentPanel/StudentPanel';
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div className='relative body'>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/student-panel' element={<StudentPanel></StudentPanel>}></Route>
+        <Route path='/student-panel' element={
+          <RequireAuth>
+            <StudentPanel></StudentPanel>
+          </RequireAuth>
+        }></Route>
         <Route path='/student-login' element={<Login></Login>}></Route>
       </Routes>
     </div>
