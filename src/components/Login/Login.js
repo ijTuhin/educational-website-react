5cf70193a531f706182e3d65363040c6296f562f
ijTuhin <isratishu0209@gmail.com>
2022-04-30 20:41:06 +0600

    alert for nonUser trying reset password

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -16,6 +16,7 @@ const Login = () => {
     const [loginState, setLoginState] = useState(false);
     const [error, setError] = useState('');
     const [reset, setReset] = useState(false);
+    const [resetError, setResetError] = useState(false);
 
     const handlePasswordInputBlur = event => {
         setPassword(event.target.value);
@@ -53,6 +54,8 @@ const Login = () => {
                 setReset(true);
             })
             .catch(error => {
+                console.error(error);
+                setResetError(true);
                 setError(error.message);
             });
     }
@@ -97,13 +100,24 @@ const Login = () => {
                                 </>
                             }
 
+                            {resetError &&
+                                <>
+                                    <div class="bg-red-100 rounded py-2.5 px-6 mb-5 text-sm text-red-700 inline-flex items-center w-full" role="alert">
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
+                                        </svg>
+                                        {error}
+                                    </div>
+                                </>
+                            }
+
                             {reset &&
                                 <>
                                     <div class="bg-green-100 rounded py-2.5 px-6 mb-5 text-sm text-green-700 inline-flex items-center w-full" role="alert">
                                         <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
                                             <path fill="currentColor" d="M504 256c0 136.967-111.033 248-248 248S8 392.967 8 256 119.033 8 256 8s248 111.033 248 248zM227.314 387.314l184-184c6.248-6.248 6.248-16.379 0-22.627l-22.627-22.627c-6.248-6.249-16.379-6.249-22.628 0L216 308.118l-70.059-70.059c-6.248-6.248-16.379-6.248-22.628 0l-22.627 22.627c-6.248 6.248-6.248 16.379 0 22.627l104 104c6.249 6.249 16.379 6.249 22.628.001z"></path>
                                         </svg>
-                                        A password reset email has been sent..
+                                        <>A password reset email has been sent..</>
                                     </div>
                                 </>
                             }
