717e498fbc3d346e5c4c142b977b9169d72bb0e5
ijTuhin <isratishu0209@gmail.com>
2022-04-30 14:29:41 +0600

    danger alert set for login error

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -21,15 +21,16 @@ const Login = () => {
         event.preventDefault();
 
         signInWithEmailAndPassword(auth, email, password)
-        .then( result => {
-            const user = result.user;
-            console.log(user);
-            alert('Successful Login');
-        })
-        .catch(error => {
-            console.error(error);
-            setError(error.message);
-        });
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                alert('Successful Login');
+            })
+            .catch(error => {
+                console.error(error);
+                setError(error.message);
+                setLoginError(true);
+            });
     }
 
     return (
@@ -43,6 +44,22 @@ const Login = () => {
                                 <img className='w-9 mb-2.5' src={iiucLogo} alt="" />
                                 <h2 className='text-3xl md:text-4xl text-blue-900 font-semibold mb-5 ml-2'>Student </h2>
                             </div>
+
+
+
+                            {loginError &&
+                                <>
+                                    <div class="bg-red-100 rounded py-2.5 px-6 mb-5 text-sm text-red-700 inline-flex items-center w-full" role="alert">
+                                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="times-circle" class="w-4 h-4 mr-2 fill-current" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8zm121.6 313.1c4.7 4.7 4.7 12.3 0 17L338 377.6c-4.7 4.7-12.3 4.7-17 0L256 312l-65.1 65.6c-4.7 4.7-12.3 4.7-17 0L134.4 338c-4.7-4.7-4.7-12.3 0-17l65.6-65-65.6-65.1c-4.7-4.7-4.7-12.3 0-17l39.6-39.6c4.7-4.7 12.3-4.7 17 0l65 65.7 65.1-65.6c4.7-4.7 12.3-4.7 17 0l39.6 39.6c4.7 4.7 4.7 12.3 0 17L312 256l65.6 65.1z"></path>
+                                        </svg>
+                                        {error}
+                                    </div>
+                                </>
+                            }
+
+
+
                             <div className="mb-3">
                                 <input onBlur={handleEmailInputBlur} type="text" className="form-control block w-72 md:w-96 px-6 py-1.5 md:text-base text-sm text-gray-700 bg-white bg-clip-padding border border-solid border-gray-400 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleFormControlInput2" name='email' placeholder="Email address" />
                             </div>
