842905bbcdc643ff4159bacceed61cc79f50b9d4
ijTuhin <isratishu0209@gmail.com>
2022-05-01 04:06:53 +0600

    student panel menu design modified

--- src/components/StudentHeader/StudentHeader.js
+++ src/components/StudentHeader/StudentHeader.js
@@ -21,19 +21,19 @@ const StudentHeader = () => {
                 <img src={iiucLogo} className="w-14" alt="" />
                 <nav className=' md:block hidden'>
                     <span className='nav'>
-                        <Link className='mx-2 px-1 py-2 hover:text-green-600' to='/'><FontAwesomeIcon className='mb-0.5' icon={faUser}></FontAwesomeIcon> Student</Link>
-                        <ul className='sub-nav bg-white mt-4 absolute top-12 left-20 px-3'>
-                            <li className='text-slate-800 hover:text-green-600 decoration-4 hover:no-underline'>Section-1</li>
-                            <li className='text-slate-800 hover:text-green-600 decoration-4 hover:no-underline'>Section-2</li>
-                            <li className='text-slate-800 hover:text-green-600 decoration-4 hover:no-underline'>Section-3</li>
-                            <li className='text-slate-800 hover:text-green-600 decoration-4 hover:no-underline'>Section-4</li>
-                            <li className='text-slate-800 hover:text-green-600 decoration-4 hover:no-underline'>Section-5</li>
+                        <Link className='mx-2 px-1 py-2' to='/'><FontAwesomeIcon className='mb-0.5' icon={faUser}></FontAwesomeIcon> Student</Link>
+                        <ul className='sub-nav bg-white/70 mt-4 absolute top-12 left-20 p-3'>
+                            <li className='text-slate-800 hover:text-blue-800'>Section-1</li>
+                            <li className='text-slate-800 hover:text-blue-800'>Section-2</li>
+                            <li className='text-slate-800 hover:text-blue-800'>Section-3</li>
+                            <li className='text-slate-800 hover:text-blue-800'>Section-4</li>
+                            <li className='text-slate-800 hover:text-blue-800'>Section-5</li>
                         </ul>
                     </span>
-                    <Link className='mx-2 px-1 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/academics'><FontAwesomeIcon className='mb-0.5' icon={faFeed}></FontAwesomeIcon> TER</Link>
-                    <Link className='mx-2 px-1 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/courses'>Application</Link>
-                    <Link className='mx-2 px-1 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/campus'>IQAC</Link>
-                    <Link className='mx-2 px-1 hover:text-green-600 hover:underline-offset-8 decoration-4 hover:underline' to='/search'>Others</Link>
+                    <Link className='mx-2 px-1 hover:underline-offset-8 decoration-2 hover:underline' to='/academics'><FontAwesomeIcon className='mb-0.5' icon={faFeed}></FontAwesomeIcon> TER</Link>
+                    <Link className='mx-2 px-1 hover:underline-offset-8 decoration-2 hover:underline' to='/courses'>Application</Link>
+                    <Link className='mx-2 px-1 hover:underline-offset-8 decoration-2 hover:underline' to='/campus'>IQAC</Link>
+                    <Link className='mx-2 px-1 hover:underline-offset-8 decoration-2 hover:underline' to='/search'>Others</Link>
                     <button onClick={handleLogOut} className='mx-2 px-1 transition duration-150 ease-in-out' to='/academics' data-bs-toggle="tooltip" data-bs-placement="bottom" title="Log Out"><FontAwesomeIcon icon={faSignOut}></FontAwesomeIcon></button>
                 </nav>
             </div>
@@ -42,7 +42,7 @@ const StudentHeader = () => {
                 <nav className='block md:hidden'>
                     <span className='nav relative'>
                         <FontAwesomeIcon className='text-3xl' icon={faBars}></FontAwesomeIcon>
-                        <ul className='sub-nav absolute top-6 right-0 border bg-white p-2 text-start'>
+                        <ul className='sub-nav absolute top-6 right-0 border bg-white/70 p-3 text-start'>
                             <li><h3 className='text-slate-800  font-thin text-sm'><FontAwesomeIcon className='mb-0.5' icon={faUserAltSlash}></FontAwesomeIcon> User</h3></li>
                             <li><Link className='text-slate-800  font-thin text-sm' to='/'><FontAwesomeIcon className='mb-0.5' icon={faUser}></FontAwesomeIcon> Student</Link></li>
                             <li><Link className='text-slate-800  font-thin text-sm' to='/academics'><FontAwesomeIcon className='mb-0.5' icon={faFeed}></FontAwesomeIcon> TER</Link></li>
